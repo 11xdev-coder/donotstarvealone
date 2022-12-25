7dd75a385bb26c4b512ed256cfb41f550cf889f4
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2022-12-25 22:04:52 +0300

    added animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,10 +4,8 @@ var p_vel = Vector2.ZERO
 const p_MAXSPEED = 25
 const v2zero = Vector2.ZERO
 
+onready var animationPlayer = $animation
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,8 +15,13 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	if input_vector != v2zero:
+		if input_vector.x > 0:
+			animationPlayer.play("walkright")
+		else:
+			animationPlayer.play("walkleft")
 		p_vel = input_vector
 	else:
+		animationPlayer.stop()
 		p_vel = v2zero
 		
 	p_vel = move_and_slide(p_vel * p_MAXSPEED)
