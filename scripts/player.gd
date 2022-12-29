b4e221ad24fac372a935adfa45d1e2b7ce5d76d8
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2022-12-29 18:19:27 +0300

    added attack animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,11 +8,26 @@ onready var animationPlayer = $animation
 onready var treeanim = $treeanim
 onready var animState = treeanim.get("parameters/playback")
 
+enum {
+	move,
+	attack
+}
+
+var state = move
+
 func _ready():
 	treeanim.active = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _process(delta):
+	match state:
+		move:
+			state_move(delta)
+		attack:
+			state_attack(delta)
+	
+	
+func state_move(delta):
 	var input_vector = v2zero
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -20,6 +35,7 @@ func _physics_process(delta):
 	if input_vector != v2zero:
 		treeanim.set("parameters/walk/blend_position", input_vector)
 		treeanim.set("parameters/idle/blend_position", input_vector)
+		treeanim.set("parameters/attack/blend_position", input_vector)
 		animState.travel("walk")
 		p_vel = input_vector
 	else:
@@ -27,3 +43,22 @@ func _physics_process(delta):
 		p_vel = v2zero
 		
 	p_vel = move_and_slide(p_vel * p_MAXSPEED)
+	
+	if Input.is_action_just_pressed("ui_attack"):
+		state = attack
+	
+func state_attack(delta):
+	p_vel = v2zero
+	animState.travel("attack")
+	
+func state_attack_finished():
+	state = move
+
+
+
+
+
+
+
+
+
