c5fe62a4521a26db904c9b9aedce70e203a40e39
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-01-18 22:26:53 +0300

    added diagonal player aniamtions

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -9,36 +9,69 @@ public class playerController : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 movement;
+    
+    public float horizontal;
+    public float vertical;
+    public bool diagonal = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void PlayAnimation()
+    {
+        if (horizontal == 0 & vertical > 0)
+        {
+            if(!diagonal)
+                animator.Play("walkbackward");
+        }
+        if (horizontal == 0 & vertical < 0)
+        {   
+            if(!diagonal)
+                animator.Play("walkfront");
+        }
+        if (vertical == 0 & horizontal > 0 || vertical != 0 & horizontal > 0)
+        {
+            animator.Play("walkright");
+        }
+        if (vertical == 0 & horizontal < 0 || vertical != 0 & horizontal < 0)
+        {
+            animator.Play("walkleft");
+        }
+    }
+
     private void Update()
     {
         // Get input from the W, A, S, and D keys
-        float horizontal = 0f;
-        float vertical = 0f;
+        horizontal = 0f;
+        vertical = 0f;
         if (Input.GetKey(KeyCode.W))
         {
             vertical = 1f;
-            animator.Play("walkbackward");
+            PlayAnimation();
         }
         if (Input.GetKey(KeyCode.A))
         {
+            diagonal = true;
             horizontal = -1f;
-            animator.Play("walkleft");
+            PlayAnimation();
         }
         if (Input.GetKey(KeyCode.S))
         {
             vertical = -1f;
-            animator.Play("walkfront");
+            PlayAnimation();
         }
         if (Input.GetKey(KeyCode.D))
         {
+            diagonal = true;
             horizontal = 1f;
-            animator.Play("walkright");
+            PlayAnimation();
+        }
+
+        if (Input.GetKeyUp(KeyCode.S) | Input.GetKeyUp(KeyCode.D))
+        {
+            diagonal = false;
         }
         movement = new Vector2(horizontal, vertical);
     }
