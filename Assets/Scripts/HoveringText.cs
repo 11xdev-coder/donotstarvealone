53d170f167832846519ba98dd47d9f6ea20c44f6
Matvei Piashchynski <66955882+11xdev-coder@users.noreply.github.com>
2023-03-05 23:14:06 +0300

    bug fix + impovements to hover code

--- Assets/Scripts/HoveringText.cs
+++ Assets/Scripts/HoveringText.cs
@@ -1,23 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class HoveringText : MonoBehaviour
 {
     public Text hoveringText;
     public InventoryManager inv;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 offset;
 
     // Update is called once per frame
     void Update()
     {
-        hoveringText.transform.localPosition = Input.mousePosition;
-        if(inv.FindClosestSlot() != null)
+        hoveringText.transform.position = Input.mousePosition + offset;
+        if (inv.FindClosestSlot() != null && inv.FindClosestSlot().item != null)
+        {
             hoveringText.text = inv.FindClosestSlot().item.name;
+        }
+        else if(!IsCursorOverHUD())
+        {
+            hoveringText.text = "Walk";
+        }
+        else
+        {
+            hoveringText.text = "";
+        }
+        print(IsCursorOverHUD());
+    }
+
+    public bool IsCursorOverHUD()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
     }
 }
